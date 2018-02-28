785bdb60823efe83b579875df6516205c37a2c19
melkotoury <melkotoury@gmail.com>
2018-02-28 13:24:05 +0200

    test: add unit test to the stack class

--- test_stack.rb
+++ test_stack.rb
@@ -1,2 +1,34 @@
-class TestStack
+require "minitest/autorun"
+require_relative 'stack'
+
+
+class TestStack < Minitest::Test
+  def test_push_pop
+    stack = Stack.new
+
+    stack.push 3
+
+    assert_equal 3, stack.pop
+  end
+
+  def test_multi_push_pop
+    stack = Stack.new
+
+    stack.push 3
+    stack.push 5
+
+    assert_equal 5, stack.pop
+    assert_equal 3, stack.pop
+  end
+
+  def test_empty
+    stack = Stack.new
+    assert stack.empty?
+
+    stack.push 3
+    refute stack.empty?
+
+    stack.pop
+    assert stack.empty?
+  end
 end
